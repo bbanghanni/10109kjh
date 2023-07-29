b150412f60c844c522bfd1046bbb117372a4feee
jihoon <ph.1onabeat@gmail.com>
2023-07-29 11:33:46 +0900

    논리연산 학습

--- pythonBasic/operator.py
+++ pythonBasic/operator.py
@@ -13,3 +13,28 @@ n = 13 // 4
 
 print(r)
 print(n)
+
+numA = 20
+numB = 4
+isEqual = numA == numB
+isnotEqual = numA != numB
+print(isEqual)
+print(isnotEqual)
+
+iaEqual = numA == 20
+
+print(isEqual)
+
+
+isBiggerThan = numA > 20
+isBiggerOrEqualThan = numA >= 20
+print(isBiggerThan)
+print(isBiggerOrEqualThan)
+
+isSmallerThan = numA < 20
+isSmallerOrEqualThan = numA <= 20
+
+print(isSmallerThan)
+print(isSmallerOrEqualThan)
+
+
