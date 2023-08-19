e9a5366abac3187fa7dcc6a2b24b55c231ca305e
jihoon <ph.1onabeat@gmail.com>
2023-08-19 10:15:43 +0900

    사용자로부터 값을 입력받는 방법

---
+++ pythonBasic/input-Variable.py
@@ -0,0 +1,6 @@
+first_number = input("숫자를 하나 입력하세요.")#input은 문자열
+temp_number = int(first_number) + 10#number를 int형으로 전환
+
+print("첫번째로는 숫자",first_number,"을 입력하셨군요! \n")
+print(first_number,"에 10을 더한 값은",temp_number,"입니다!")
+second_number = int(input("다음 숫자도 입력해볼까요?"))#이런형식으로치환가능
--- pythonBasic/printVariable.py
+++ pythonBasic/printVariable.py
@@ -8,3 +8,6 @@ print(first_variable)
 print(second_variable)
 print("변수에 넣지 않고 출력을 할거예요")
 print(1)
+
+trueValue = True
+falseValue = False
