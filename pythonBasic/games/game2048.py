e7560230f9bc6e9448b2784709b11841d63eb2e5
jihoon <ph.1onabeat@gmail.com>
2023-11-18 11:54:33 +0900

    키 이벤트 설정 및 배경 그리기

--- pythonBasic/games/game2048.py
+++ pythonBasic/games/game2048.py
@@ -2,6 +2,7 @@ import pygame
 
 colors = {'white':(255, 255, 255)
          ,'black': (0 ,0 ,0),
+          '-1':(200, 200,200),
           '2':(251, 233, 231),
           '4':(255, 204, 188),
           '8':(255, 171, 145),
@@ -14,20 +15,50 @@ colors = {'white':(255, 255, 255)
           '1024':(191, 54, 12),
           '2048':(213, 0, 0)}
 
+board = [[-1,-1,-1,-1],
+         [-1,-1,-1,-1],
+         [-1,-1,-1,-1],
+         [-1,-1,-1,-1]]
+
+isGameRunning = True
+screensize = (500, 500)
+screen = pygame.display.set_mode(screensize)
+
 def initScreen():
-    size = (500, 500)
-    screen = pygame.display.set_mode(size)
     screen.fill(colors['white'])
     pygame.display.update()
 
-isGameRunning = True
+def setEventListener():
+    global isGameRunning
+    for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_q:
+                isGameRunning = False
+            else:
+                print("키보드 키 입력 이벤트가 감지됨")
+
+def drawDisplay():
+    global screen
+    baseX = 35
+    baseY = 35
+    blockHeight = 100
+    blockWidth = 100
+    margin = 10
+    for i in range(4):
+        for j in range(4):
+            x = (blockWidth + margin) * j + baseX
+            y = (blockHeight + margin) * i + baseY
+            pygame.draw.rect(screen, colors['-1'], [x, y, blockWidth, blockHeight],2)
+    pygame.display.flip()
 
 def run2048():
     pygame.init()
     initScreen()
     print("2048 게임 시작")
     while isGameRunning:
-        pass
+        setEventListener()
+        drawDisplay()
+
     pygame.quit()
 
 run2048()
